bf3b2e3612124b05663bb54a1a41a19cfb3b32af
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2024-07-19 23:32:29 +0400

    final project last changes

--- src/components/AppoinmentForm/Form.jsx
+++ src/components/AppoinmentForm/Form.jsx
@@ -1,47 +1,54 @@
-import '../AppoinmentForm/Form.css'
+import '../AppoinmentForm/Form.css';
+import { MdOutlineArrowDropDown } from "react-icons/md";
 
 const Form = () => {
     return (
         <div className="contact-form-content">
-           <div className="contact-titles">
-            <div>
-            <h5 className='contact-form-title'>Contact us</h5>
-            <h2>Make an <br/>Appoinment</h2>
+            <div className="contact-titles">
+                <div>
+                    <h5 className='contact-form-title'>Contact us</h5>
+                    <h2>Make an <br />Appoinment</h2>
+                </div>
+
             </div>
-         
-           </div>
-            
+
             <div className="contact-form-general">
-            <div className="contact-form-items">
-                <div className="name-email">
-                    <input type="text" placeholder='Full Name' />
-                    <input type="email" placeholder='example@gmail.com' />
+                <div className="contact-form-items">
+                    <div className="name-email">
+                        <input type="text" placeholder='Full Name' />
+                        <input type="email" placeholder='example@gmail.com' />
+                    </div>
+                    <div className="department-time">
+                        <div className="select-wrapper">
+                            <select name="department" id="department">
+                                <option selected>Please Select</option>
+                                <option value="sales">Sales</option>
+                                <option value="marketing">Marketing</option>
+                                <option value="ıt">IT</option>
+                                <option value="accounting">Accounting</option>
+                            </select>
+                            <MdOutlineArrowDropDown className='dropdown' />
+                        </div>
+
+
+                        <div className="select-wrapper">
+                            <select name="time" id="time">
+                                <option selected>4:00 Available</option>
+                                <option value="3:20">3:20</option>
+                                <option value="7:30">7:30</option>
+                            </select>
+                            <MdOutlineArrowDropDown className='dropdown' />
+                            </div>
+                    </div>
+                    <textarea name="message" id="message" placeholder='Message'></textarea>
+
+                    <div className="contact-btn">
+                        <input class="contact-btn" type="button" value="Book Appointment" />
+
+                    </div>
                 </div>
-                <div className="department-time">
-
-                    <select name="department" id="department">
-                        <option selected>Please Select</option>
-                        <option value="sales">Sales</option>
-                        <option value="marketing">Marketing</option>
-                        <option value="ıt">IT</option>
-                        <option value="accounting">Accounting</option>
-                    </select>
-
-                    <select name="time" id="time">
-                        <option selected>4:00 Available</option>
-                        <option value="3:20">3:20</option>
-                        <option value="7:30">7:30</option>
-                    </select>
-                </div>
-               <textarea name="message" id="message"  placeholder='Message'></textarea>
-
-               <div className="contact-btn">
-               <input class="contact-btn" type="button" value="Book Appointment" />
-
-               </div>
             </div>
-            </div>
-            
+
         </div>
     )
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from 'react';
 import { useFormik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContent, getAppointment } from '../../redux/reducers/appointmentSlice';
-
+import { MdOutlineArrowDropDown } from "react-icons/md";
 
 
 const Form = () => {
   const dispatch = useDispatch();
 
- const [toast, setToast] = useState('');
- const [success, setSucces] = useState('');
+  const [toast, setToast] = useState('');
+  const [success, setSucces] = useState('');
 
   const formik = useFormik({
     initialValues: {
@@ -33,13 +33,13 @@ const Form = () => {
           .then((res) => res.json())
           .then((result) => dispatch(getAppointment(result)))
           .catch(error => console.log(error.message));
-          values.name = '';
-          values.email = '';
-          values.department = '',
-          values.time = ''       
+        values.name = '';
+        values.email = '';
+        values.department = '',
+          values.time = ''
       }
 
-   
+
 
     },
 
@@ -55,46 +55,54 @@ const Form = () => {
 
 
   return (
-    <div className ="form-layout">
-   <h3 className='home-form-title'>Book Appointment</h3> 
-   <div className='toast-messages'>
-   <p className='toast-msg'>{toast}</p>
-   <p className='success-msg'>{success}</p>
-   </div>
-
-     <form  onSubmit={formik.handleSubmit}>
-      <div className="form-content">
-    <label htmlFor="name">Name*</label>
-    <input type="text" id="name" name="name" value={formik.values.name}
-        onChange={formik.handleChange}  placeholder='Full Name'    {...formik.getFieldProps("name")}/>
-
-    <label htmlFor="email">Email*</label>
-    <input type="email" id="email" name="email" onChange={formik.handleChange}
-        value={formik.values.email}placeholder='example@gmail.com'    {...formik.getFieldProps("email")}/>
- 
-    <label htmlFor="department">Department*</label>
-    <select name="department" id="department" value={formik.values.department} onChange={formik.handleChange}  {...formik.getFieldProps("department")}>
-    <option value="Please Select">Please Select</option>
-        <option value="sales">Sales</option>
-        <option value="marketing">Marketing</option>
-        <option value="ıt">IT</option>
-        <option value="accounting">Accounting</option>
-    </select>
-   
-
-    <label htmlFor="time">Time*</label>
-    <select name="time" id="time" value={formik.values.time}
-        onChange={formik.handleChange}  {...formik.getFieldProps("time")}>
-    <option value="4:00 Available">4:00 Available</option>
-        <option value="3:20">3:20</option>
-        <option value="7:30">7:30</option>
-    </select>
-
-
-    <button className='home-form-btn'>Book Appointment</button>
+    <div className="form-layout">
+      <h3 className='home-form-title'>Book Appointment</h3>
+      <div className='toast-messages'>
+        <p className='toast-msg'>{toast}</p>
+        <p className='success-msg'>{success}</p>
       </div>
-    
-     </form>
+
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-content">
+          <label htmlFor="name">Name*</label>
+          <input type="text" id="name" name="name" value={formik.values.name}
+            onChange={formik.handleChange} placeholder='Full Name'    {...formik.getFieldProps("name")} />
+
+          <label htmlFor="email">Email*</label>
+          <input type="email" id="email" name="email" onChange={formik.handleChange}
+            value={formik.values.email} placeholder='example@gmail.com'    {...formik.getFieldProps("email")} />
+
+
+
+          <label htmlFor="department">Department*</label>
+          <div className="custom-select-wrapper">
+            <select name="department" id="department" value={formik.values.department} onChange={formik.handleChange}  {...formik.getFieldProps("department")}>
+              <option value="Please Select">Please Select</option>
+              <option value="sales">Sales</option>
+              <option value="marketing">Marketing</option>
+              <option value="ıt">IT</option>
+              <option value="accounting">Accounting</option>
+
+            </select>
+            <MdOutlineArrowDropDown className='dropdown' />
+          </div>
+
+
+          <label htmlFor="time">Time*</label>
+          <div className="custom-select-wrapper">
+            <select name="time" id="time" value={formik.values.time}
+              onChange={formik.handleChange}  {...formik.getFieldProps("time")}>
+              <option value="4:00 Available">4:00 Available</option>
+              <option value="3:20">3:20</option>
+              <option value="7:30">7:30</option>
+            </select>
+            <MdOutlineArrowDropDown className='dropdown' />
+          </div>
+
+          <button className='home-form-btn'>Book Appointment</button>
+        </div>
+
+      </form>
     </div>
 
   )
